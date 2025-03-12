38838f5f57308af5628fe1eb72f180729984fb5b
Sabrina <sabis476@gmail.com>
2025-03-12 20:41:53 -0300

    Primeiro commit

---
+++ src/main/java/com/java/odontovisionMVC/OdontovisionMvcApplication.java
@@ -0,0 +1,13 @@
+package com.java.odontovisionMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontovisionMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontovisionMvcApplication.class, args);
+	}
+
+}
