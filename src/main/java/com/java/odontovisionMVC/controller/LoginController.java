38eddc2490519d06baac438ee66b9e0784782a2a
Sabrina <sabis476@gmail.com>
2025-03-13 14:36:20 -0300

    Ajustando login

--- src/main/java/com/java/odontovisionMVC/controller/LoginController.java
+++ src/main/java/com/java/odontovisionMVC/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.java.odontovisionMVC.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,10 +12,14 @@ public class LoginController {
     @PostMapping("/login")
     public String login(@RequestParam String usuario, @RequestParam String senha, Model model) {
         if ("admin123".equals(usuario) && "admin123".equals(senha)) {
-            return "redirect:/painel"; // Redireciona para o painel do administrador
-        } else {
-            model.addAttribute("erro", "Usuário ou senha incorretos!");
-            return "login_admin"; // Retorna à página de login com mensagem de erro
+            return "redirect:/painel_admin"; // Redireciona para o painel administrativo
         }
+        model.addAttribute("erro", "Usuário ou senha incorretos!");
+        return "login"; // Retorna para a página de login em caso de erro
+    }
+
+    @GetMapping("/login")
+    public String mostrarLogin() {
+        return "login";
     }
 }
