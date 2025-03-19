09e2defc7f96b2db046cdf394154857b9b40171c
sabrinacafe <sabis476@gmail.com>
2025-03-19 12:30:43 -0300

    Apagando importacao desnecessária

--- src/main/java/com/java/odontovisionMVC/controller/UsuarioController.java
+++ src/main/java/com/java/odontovisionMVC/controller/UsuarioController.java
@@ -3,7 +3,6 @@ package com.java.odontovisionMVC.controller;
 import com.java.odontovisionMVC.model.Usuario;
 import com.java.odontovisionMVC.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
